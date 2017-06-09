38aba352b96f6b6e8f9a44fcaf24ecc780464092
AntonDachauer <adachauer@webworks-nuernberg.de>
2017-06-09 16:40:40 +0200

    Translations for the backend

--- Form/PostType.php
+++ Form/PostType.php
@@ -25,42 +25,42 @@ class PostType extends AbstractType
         $builder
             ->add('title', TextType::class, [
                 'required' => true,
-                'label' => 'Title'
+                'label' => 'backend.form.title',
             ])
             ->add('content', TextareaType::class, [
                 'required' => true,
-                'label' => 'Content'
+                'label' => 'backend.form.content',
             ])
             ->add('status', ChoiceType::class, [
                 'required' => true,
-                'label' => 'Status',
+                'label' => 'backend.form.status',
                 'choices' => [
-                    'Draft' => BlogService::POST_STATUS_DRAFT,
-                    'Published' => BlogService::POST_STATUS_PUBLISHED,
-                    'Deleted' => BlogService::POST_STATUS_DELETED,
+                    'backend.form.status.draft' => BlogService::POST_STATUS_DRAFT,
+                    'backend.form.status.published' => BlogService::POST_STATUS_PUBLISHED,
+                    'backend.form.status.deleted' => BlogService::POST_STATUS_DELETED,
                 ]
             ])
             ->add('comments_enabled', ChoiceType::class, [
                 'required' => true,
-                'label' => 'Comments enabled',
+                'label' => 'backend.form.comments_enabled',
                 'expanded' => true,
                 'multiple' => false,
                 'choices' => [
                     'Yes' => 1,
                     'No' => 0,
-                ]
+                ],
             ])
             ->add('categories', EntityType::class, [
                 'class' => Category::class,
                 'multiple' => true,
                 'expanded' => true,
-                'choice_label' => 'title',
+                'label' => 'backend.form.categories',
             ])
             ->add('tags', EntityType::class, [
                 'class' => Tag::class,
                 'multiple' => true,
                 'expanded' => true,
-                'choice_label' => 'title',
+                'label' => 'backend.form.tags',
                 'query_builder' => function (EntityRepository $er) {
                     return $er->createQueryBuilder('t')
                         ->where('t.status = :status')
